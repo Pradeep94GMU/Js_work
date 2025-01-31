61f106e6196fadff558be4ef209c14d120278719
Pradeep Ranjan <111266003+Pradeep94GMU@users.noreply.github.com>
2025-01-31 16:39:26 +0000

    varibales done

---
+++ basics/vari.js
@@ -0,0 +1,9 @@
+const accontId = 123333
+let accountPassword = "123456"
+var accountCity = "Chapra"
+accountState ="Bihar"
+let accountNum;
+
+console.log(accontId);
+
+console.table([accontId, accountCity,accountNum,accountPassword, accountState])
