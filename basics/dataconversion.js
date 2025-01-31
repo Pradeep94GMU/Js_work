015f765ae47543e1f89e155bfeb046d85b7acc35
Pradeep Ranjan <111266003+Pradeep94GMU@users.noreply.github.com>
2025-01-31 18:10:07 +0000

    updated conver

--- basics/dataconversion.js
+++ basics/dataconversion.js
@@ -6,6 +6,25 @@ score = undefined
 // for null -> 0
 //if able to convert then it wil retirn a number else it will return a NaN. 
 
-console.log(Number(score)) //Number() is a wrapper class.
+// console.log(Number(score)) //Number() is a wrapper class.
 
 //conversion using wrapper class hota hai yaha...
+
+console.log(1 + "2" + 1);
+console.log("12" + 1 + "2" + 1);
+console.log("2" + 1 + 45 + 22 + 71);
+console.log(2 + 2 * 3 % 3);
+
+let x = 100
+
+let y = ++x
+let z = x++
+z = x++
+z = ++x
+console.log(z);
+console.log(x);
+
+// console.log((x++) + (++y)) //101 + 101 = 202
+
+
+
--- basics/vari.js
+++ basics/vari.js
@@ -6,4 +6,9 @@ let accountNum;
 
 console.log(accontId);
 
+/*
+Recommendation to not to use the var
+coz we are not to handle the issue of score of block vs funtion
+*/
+
 console.table([accontId, accountCity,accountNum,accountPassword, accountState])
