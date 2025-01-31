b090ffe8de634e7f601860b6f54efd4ed9ee73ac
Pradeep Ranjan <111266003+Pradeep94GMU@users.noreply.github.com>
2025-01-31 17:22:35 +0000

    conversion updated

---
+++ basics/dataconversion.js
@@ -0,0 +1,11 @@
+let score = 123
+
+score = undefined
+
+//for undefined: Number() -> return NaN
+// for null -> 0
+//if able to convert then it wil retirn a number else it will return a NaN. 
+
+console.log(Number(score)) //Number() is a wrapper class.
+
+//conversion using wrapper class hota hai yaha...
